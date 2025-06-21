dd885a0879c0a8ddd5b91290a1481db632393d79
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-06-21 09:31:34 +0330

    Form submittion to database added

--- job_application/forms.py
+++ job_application/forms.py
@@ -4,10 +4,6 @@ from django import forms
 class ApplicationForm(forms.Form):
     """docstring for ApplicationForm."""
 
-    def __init__(self, arg):
-        super(ApplicationForm, self).__init__()
-        self.arg = arg
-
     first_name = forms.CharField(max_length=80)
     last_name = forms.CharField(max_length=80)
     email = forms.EmailField()
--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
 
 
 def index(request):
@@ -11,5 +13,16 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
-            print(first_name)
+
+            # save data on database using model
+            Form.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                date=date,
+                occupation=occupation
+            )
+
+            messages.success(request, "Form submitted successfully.")
+            
     return render(request, "index.html")
