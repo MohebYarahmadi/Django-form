5f8a2a7fb5a3a38567acc007ab8321891ab53e39
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-06-24 12:13:44 +0330

    Base template adde

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+        "date"
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+        "date"
+    )
+    list_filter = (
+        "date", "occupation"
+    )
+    ordering = ("date",)
+    readonly_fields = ("occupation",)
+
+admin.site.register(Form, FormAdmin)
